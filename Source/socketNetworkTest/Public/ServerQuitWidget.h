bb78846258e0ad4b072f3cfdb00820cd8e8a770c
yunjulanju <yunju6528@duksung.ac.kr>
2024-04-05 16:23:16 +0900

    버튼으로 서버 끌 수 있음

---
+++ Source/socketNetworkTest/Public/ServerQuitWidget.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ServerQuitWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOCKETNETWORKTEST_API UServerQuitWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+
+	UFUNCTION(BlueprintCallable)
+	void OnButtonClicked();
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* QuitButton;
+};
--- Source/socketNetworkTest/socketNetworkTest.Build.cs
+++ Source/socketNetworkTest/socketNetworkTest.Build.cs
@@ -8,6 +8,6 @@ public class socketNetworkTest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Networking", "Sockets" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Networking", "Sockets", "UMG" });
 	}
 }
