449ec2252c3af64defaab0f3d29251cb11ca8ee8
lsh210726 <136397554+lsh210726@users.noreply.github.com>
2024-04-05 16:38:53 +0900

    수박게임블프구현

---
+++ Source/socketNetworkTest/Private/LSH_fruit.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LSH_fruit.h"
+
+// Sets default values
+ALSH_fruit::ALSH_fruit()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALSH_fruit::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALSH_fruit::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
